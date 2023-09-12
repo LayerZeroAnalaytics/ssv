d13d1f07f1f32f7e962507e187e24abab1021937
Til Jordan <45370031+tiljrd@users.noreply.github.com>
2023-09-12 21:10:39 +0100

    Add libp2p

--- network/p2p/p2p_setup.go
+++ network/p2p/p2p_setup.go
@@ -18,6 +18,7 @@ import (
 	"github.com/bloxapp/ssv/network/streams"
 	"github.com/bloxapp/ssv/network/topics"
 	"github.com/bloxapp/ssv/utils/commons"
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	libp2pdiscbackoff "github.com/libp2p/go-libp2p/p2p/discovery/backoff"
 	basichost "github.com/libp2p/go-libp2p/p2p/host/basic"
